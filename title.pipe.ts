c69a08922726ff879d8b1b1dc15e98bdfefec2b9
Hemal <88051836+hemal-divyang@users.noreply.github.com>
2023-08-08 20:20:08 +0530

    Update title.pipe.ts

--- title.pipe.ts
+++ title.pipe.ts
@@ -1,23 +1,37 @@
+//PIPE
+
+//pipes allow us to change the data before display
+// inbuild pipes are: 
+Uppercase - ex. {{user.id | uppercase}}
+Lowercase - ex. {{user.name | lowercase}}
+titlecase - ex. {{user.city | uppercase}}
+Date - ex. {{user.date | date:'dd/mm/yyyy}}
+Currency - ex. {{user.salary | currency:'INR':'3.3-3'}}
+Percent - ex. {{user.result | percent}}
+Number 
+Slice - ex. {{user.id | slice:0:1}}
+Json - ex. {{user|json}}
+Async - to handle asynchronus data
+
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'title'
+  name: 'age'
 })
-export class TitlePipe implements PipeTransform {
+export class AgePipe implements PipeTransform {
 
-  transform(name: string, gender: string): unknown {
-    if (gender === 'female') {
-      return 'mrs' + ' ' + name;
-    } else {
-      return 'mr' + ' ' + name
-    }
-  }
+  transform(value:any,args?:any){
+	let curetnYear = new Date().getFullYear();
+	let userBirthYear = new Date(value).getFullYear();
+	let userAge = curetnYear - userBirthYear;
+	return userAge;
+}
 
 }
 
 //in html
-<!--show mr / mrs before name-->
+<!--show user age-->
 
 <div *ngFor="let p of person">
-    {{p.name | title : p.gender}}
+    {{p.dob | age}}
 </div>
