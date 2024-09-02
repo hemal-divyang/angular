809da8db7d852009d983e20935541da03060c43a
Hemal <88051836+hemal-divyang@users.noreply.github.com>
2024-09-02 11:59:36 -0400

    Update subject.ts

--- subject.ts
+++ subject.ts
@@ -1,27 +1,16 @@
 SUBJECT IN ANGULAR
 
-1. create service file (notificationService)
-   import subject from rxjs
-   create subject : public notifySubject = new Subject();
-   create function : sendNotification(data){
-						this.notifySubject.next(data)
-					}
+public notifySubject = new Subject();
+sendNotification(data){
+  this.notifySubject.next(data)
+}
    
-2. pass the data in component(ex contact component in input tag)
-   import notificationService
-   inject in constructor(create instance of service)
-   create one input in html with refrence variable
-   create button with click event, pass reference variable to event
-   click event : sendMessage(data){
-					this.notifyService.sendNotification(data.value)
-					}
+sendMessage(data){
+  this.notifyService.sendNotification(data.value)
+}
 
-3. read the data in component(ex notification component)
-   import notificationService
-   inject in constructor(create instance of service)
-   create variable to read data (notificationMessage:string)
-   ngOnInit(){
+ngOnInit(){
    this.notifyService.notifySubject.subscribe(res=>{
-	this.notificationMessage = res
-   })
-   }
+   this.notificationMessage = res
+})
+}
